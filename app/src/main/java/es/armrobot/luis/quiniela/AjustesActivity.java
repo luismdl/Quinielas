a816369fccc150af03f36ae03282580a43b49885
Luis Martin <luis_martindelarubia@hotmail.com>
2017-10-09 09:24:09 +0200

    todo

---
+++ app/src/main/java/es/armrobot/luis/quiniela/AjustesActivity.java
@@ -0,0 +1,50 @@
+package es.armrobot.luis.quiniela;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class AjustesActivity extends AppCompatActivity {
+
+    EditText p1,px;
+    Button aceptar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajustes);
+        p1=(EditText) findViewById(R.id.editText_p1);
+        px=(EditText) findViewById(R.id.editText_px);
+        aceptar=(Button) findViewById(R.id.botonAceptar);
+    }
+
+    public void aceptar(View view) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast;
+        if (p1.getText().toString().equals("") || px.getText().toString().equals("")) {
+            toast=Toast.makeText(context, R.string.textEmpty, duration);
+            toast.show();
+        } else {
+            Double p1d=Double.parseDouble(p1.getText().toString());
+            Double pxd=Double.parseDouble(px.getText().toString());
+            if (p1d + pxd > 1.0){
+                toast=Toast.makeText(context, R.string.text1, duration);
+                toast.show();
+            }else{
+                Intent returnIntent = new Intent();
+                Bundle data = new Bundle();
+                data.putDouble("p1",p1d);
+                data.putDouble("px",pxd);
+                returnIntent.putExtras(data);
+                setResult(RESULT_OK,returnIntent);
+                finish();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/es/armrobot/luis/quiniela/MainActivity.java
@@ -0,0 +1,95 @@
+package es.armrobot.luis.quiniela;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Quiniela quiniela;
+    //componentes graficos
+    TextView textView_informacion;
+    Button botonGenerar, botonLimpiar, botonAjustes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //inicializa quiniela
+        quiniela=new Quiniela(0.6,0.2,0.25,0.25,0.25);
+        //inicializa botones
+        textView_informacion=(TextView)findViewById(R.id.textView_informacion);
+        botonAjustes=(Button)findViewById(R.id.botonAjustes);
+        botonGenerar=(Button)findViewById(R.id.botonGenerar);
+        botonLimpiar=(Button)findViewById(R.id.botonLimpiar);
+
+    }
+
+    public void botones(View view){
+        switch (view.getId()) {
+            case R.id.botonGenerar:
+                String quiniel = quiniela.dameQuiniela();
+                SpannableString sb = new SpannableString( quiniel );
+                for(int i=0;i<quiniel.length()-4;i++){
+                    if(quiniel.charAt(i)=='1') {
+                        sb.setSpan(new ForegroundColorSpan(Color.GREEN), i, i+1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    }else if(quiniel.charAt(i)=='2'){
+                        sb.setSpan(new ForegroundColorSpan(Color.RED),i,i+1,Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    }else if(quiniel.charAt(i)=='X'){
+                        sb.setSpan(new ForegroundColorSpan(Color.BLACK),i,i+1,Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    }
+                }
+                textView_informacion.setText(sb);
+                break;
+            case R.id.botonLimpiar:
+                textView_informacion.setText(R.string.text_main);
+                break;
+            case R.id.botonAjustes:
+                Intent ajustes = new Intent(this, AjustesActivity.class);
+                startActivityForResult(ajustes,1);
+                break;
+        }
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK){
+                Double p1 = data.getDoubleExtra("p1",0.4);
+                Double px = data.getDoubleExtra("px",0.2);
+                Log.d("p1","p1: "+p1);
+                Log.d("px","px: "+px);
+                quiniela.setProbabilidades(p1,px);
+            }
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(R.id.ajustesIcon==item.getItemId()){
+            botones(botonAjustes);
+        }
+        return true;
+
+    }
+}
