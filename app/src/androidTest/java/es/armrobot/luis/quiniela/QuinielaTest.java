59b3d34d2331ab735d5ddb67908614887c436562
Luis Martin <luis_martindelarubia@hotmail.com>
2017-10-10 09:31:47 +0200

    Quiniela test inicial

---
+++ app/src/androidTest/java/es/armrobot/luis/quiniela/QuinielaTest.java
@@ -0,0 +1,16 @@
+package es.armrobot.luis.quiniela;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Luis on 10/10/17.
+ */
+public class QuinielaTest {
+    @Test
+    public void dameQuiniela() throws Exception {
+
+    }
+
+}
